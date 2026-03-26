3f24c6dc920e2e899ce573076b81fc5022405b04
Aditi Chandra <aditichandra126@gmail.com>
2026-03-26 16:06:08 +0530

    1594. Maximum Non Negative Product in a Matrix

---
+++ MaxNonNegativeProd.java
@@ -0,0 +1,64 @@
+
+
+class MaxNonNegativeProd {
+
+    public static void main(String[] args) {
+        MaxNonNegativeProd obj = new MaxNonNegativeProd();
+
+        int[][] grid = {
+            {1, -2, 1},
+            {1, -2, 1},
+            {3, -4, 1}
+        };
+
+        int result = obj.maxProductPath(grid);
+        System.out.println("Max Non-Negative Product: " + result);
+    }
+
+    public int maxProductPath(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        final int MOD = 1_000_000_007;
+
+        long[][] Min = new long[m][n];
+        long[][] Max = new long[m][n];
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++) {
+                Min[i][j] = Long.MAX_VALUE;
+                Max[i][j] = Long.MIN_VALUE;
+            }
+
+        Min[0][0] = Max[0][0] = grid[0][0];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) 
+                    continue;
+
+                long mn = Long.MAX_VALUE;
+                long mx = Long.MIN_VALUE;
+
+                if (i > 0) {
+                    long a = Min[i-1][j] * grid[i][j];
+                    long b = Max[i-1][j] * grid[i][j];
+                    mn = Math.min(mn, Math.min(a, b));
+                    mx = Math.max(mx, Math.max(a, b));
+                }
+
+                if (j > 0) {
+                    long a = Min[i][j-1] * grid[i][j];
+                    long b = Max[i][j-1] * grid[i][j];
+                    mn = Math.min(mn, Math.min(a, b));
+                    mx = Math.max(mx, Math.max(a, b));
+                }
+
+                Min[i][j] = mn;
+                Max[i][j] = mx;
+            }
+        }
+
+        long res = Max[m-1][n-1];
+        return res >= 0 ? (int)(res % MOD) : -1;
+    }
+}
