8a1bce840c868cbc64262ed0a29f02a811ca8549
Aditi Chandra <aditichandra126@gmail.com>
2026-04-24 11:05:26 +0530

    417. Pacific Atlantic Water Flow

---
+++ PacificAtlanticWaterFlow.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+public class PacificAtlanticWaterFlow {
+
+    private int m, n;
+    private int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        m = heights.length;
+        n = heights[0].length;
+
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+
+        for (int j = 0; j < n; j++) {
+            dfs(0, j, heights, pacific);
+        }
+
+        for (int i = 0; i < m; i++) {
+            dfs(i, 0, heights, pacific);
+        }
+
+        for (int j = 0; j < n; j++) {
+            dfs(m - 1, j, heights, atlantic);
+        }
+
+        for (int i = 0; i < m; i++) {
+            dfs(i, n - 1, heights, atlantic);
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    result.add(Arrays.asList(i, j));
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private void dfs(int i, int j, int[][] heights, boolean[][] visited) {
+        if (visited[i][j]) return;
+
+        visited[i][j] = true;
+
+        for (int[] d : directions) {
+            int x = i + d[0];
+            int y = j + d[1];
+
+            if (x < 0 || x >= m || y < 0 || y >= n) continue;
+            if (heights[x][y] < heights[i][j]) continue;
+
+            dfs(x, y, heights, visited);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] heights = {
+            {1, 2, 2, 3, 5},
+            {3, 2, 3, 4, 4},
+            {2, 4, 5, 3, 1},
+            {6, 7, 1, 4, 5},
+            {5, 1, 1, 2, 4}
+        };
+
+        PacificAtlanticWaterFlow obj = new PacificAtlanticWaterFlow();
+
+        List<List<Integer>> result = obj.pacificAtlantic(heights);
+
+        System.out.println("Cells where water can flow to both oceans:");
+        System.out.println(result);
+    }
+}
